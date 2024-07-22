6b19d6afcca8b56bfa158c44cb82d32f111148cd
Vintancion <Vitalikarshinov1122@gmail.com>
2024-07-22 17:48:00 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,6 @@
+course = 'python' 
+namber_of_hw = 12
+time = 1.5
+#watch = '0.125'
+example = (time / namber_of_hw)
+print('название курса:', course,',', 'всего задач:', namber_of_hw,',', 'затрачено часов:', time,',', 'среднее время выполнения:', example, 'часа')
